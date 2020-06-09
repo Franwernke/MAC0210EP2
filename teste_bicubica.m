e73ef9f9513ea2545268c7f5050497a114cec4ac
Francisco Eugênio Wernke <franwerke@usp.br>
2020-06-08 22:47:23 -0300

    Bicubic working for 1 square

--- teste_bicubica.m
+++ teste_bicubica.m
@@ -33,13 +33,12 @@ endfunction
 function result = p(a, x, y)
   result = 0;
   i = 1;
-  j = 1;
   while(i <= 4)
+    j = 1;
     while(j <= 4)
       result = result + a(i, j)*(x^(i-1))*(y^(j-1));
       j = j + 1;
     endwhile
-    j = 1;
     i = i + 1;
   endwhile
   return;
@@ -48,19 +47,12 @@ endfunction
 x = input("Digite a coordenada x: \n");
 y = input("Digite a coordenada y: \n");
 
-i = 0;
-while(i - x < 0)
-  i = i + 1;
-endwhile
-x_2 = i;
-x_1 = i - 0.1;
 
-i = 0;
-while(i - y < 0)
-  i = i + 1;
-endwhile
-y_2 = i;
-y_1 = i - 0.1;
+x_2 = ceil(x);
+x_1 = x_2 - 1;
+
+y_2 = ceil(y);
+y_1 = y_2 - 1;
 
 matriz_fs = [f(x_1, y_1), f(x_1, y_2), f_y(x_1, y_1), f_y(x_1, y_2);...
              f(x_2, y_1), f(x_2, y_2), f_y(x_2, y_1), f_y(x_2, y_2);...
@@ -68,32 +60,32 @@ matriz_fs = [f(x_1, y_1), f(x_1, y_2), f_y(x_1, y_1), f_y(x_1, y_2);...
              f_x(x_2, y_1), f_x(x_2, y_2), f_y_x(x_2, y_1), f_y_x(x_2, y_2)];
              
 B = [1, 0, 0, 0;...
-     1, 0.00000001, 0.00000001, 0.00000001;...
-     0, 1, 0, 0;...
-     0, 1, 0.00000002, 0.00000003];
+     0, 0, 1, 0;...
+     -3, 3, -2, -1;...
+     2, -2, 1, 1];
 
-a = inv(B)*matriz_fs*inv(transpose(B));
+a = B*matriz_fs*transpose(B);
 
 matriz_f_1 = zeros(200, 200);
 matriz_a_1 = zeros(200, 200);
 
-file_id = fopen("arq2", "w");
+#{
+  The given matrix 200x200 is going to form an image, by iterating for f(x, y)
+  and p(x, y) we have the interpolated matrix and the original one.
+  We begin at the bottom left of the image, then go to the right completing the
+  row where we stand and every 200 pixels we change the row up.  
+#}
 
 i = 1;
-j = 1;
 h = 0.005;
 while(i <= 200)
+  j = 1;
   while(j <= 200)
     matriz_f_1(201-i, j) = f((x_1 + (j-1)*h), (y_1 + (i-1)*h));
-    matriz_a_1(201-i, j) = p(a, (x_1 + (j-1)*h), (y_1 + (i-1)*h));
-    fprintf(file_id, "%.3f ", matriz_a_1(201-i, j));
+    matriz_a_1(201-i, j) = p(a, ((x_1 + (j-1))*h), ((y_1 + (i-1))*h));
     j = j + 1;
   endwhile
-  j = 1;
-  fprintf(file_id, "\n");
   i = i + 1;
 endwhile
-
-fclose(file_id);
 
 imagesc(matriz_f_1);
